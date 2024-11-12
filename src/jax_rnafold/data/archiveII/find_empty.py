1340881d4be2ac461ff134bc1ce0b971b1608b6a
Ryan Krueger <ryankrue@umich.edu>
2024-11-12 01:04:19 -0500

    Cleaning up data

--- src/jax_rnafold/data/archiveII/find_empty.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-for filename in sys.argv[1:]:
-    with open(filename) as f:
-        lines = [x for x in f]
-    if all([x.split()[4]=="0" for x in lines[1:]]):
-        print filename
-
